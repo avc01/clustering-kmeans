d026904cef367f184ee845b0db39565480bc3307
Andrés Víquez <73793020+avc01@users.noreply.github.com>
2022-08-20 17:24:46 -0600

    Initial commit

---
+++ clustering-kmeans-penguins.R
@@ -0,0 +1,22 @@
+# Se instala el paquete.
+install.packages("palmerpenguins")
+
+# Se cargan las librerias a utilizar
+library(palmerpenguins)
+library(factorextra)
+
+# Para visualizar los datasets que incluye el paquete.
+data(package = 'palmerpenguins')
+
+# Resumen de las observaciones y variables del dataset.
+summary(penguins)
+
+# Se carga la data dentro de una variable.
+test <- data(penguins)
+
+
+
+
+
+
+
